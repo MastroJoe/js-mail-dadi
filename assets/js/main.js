1576b0236da770ec33c74f31e940537b0ca543bc
Giorgio <giorgio.pignattari@gmail.it>
2021-03-02 15:53:56 +0100

    creati Input e Array

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
+// Mail
+
 // INPUT
+// mail utente
+var mail = prompt("Inserisci la tua mail:");
+console.log(mail);
+
+// array mail utenti
+var listaMail = ["random.mail@libero.it", "random.mail@gmail.com", "random.mail@yahoo.it"];
+console.log(listaMail);
