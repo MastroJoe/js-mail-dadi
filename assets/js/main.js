af59b3809ac93c42038fee5cdc3d3f39280cbbca
Giorgio <giorgio.pignattari@gmail.it>
2021-03-02 16:10:29 +0100

    controllo array e stampa su console

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,21 @@
 // Mail
-
 // INPUT
 // mail utente
 var mail = prompt("Inserisci la tua mail:");
 console.log(mail);
 
+// OUTPUT
 // array mail utenti
-var listaMail = ["random.mail@libero.it", "random.mail@gmail.com", "random.mail@yahoo.it"];
+var listaMail = ["random.mail@libero.it", "random.mail@gmail.com", "random.mail@yahoo.com", "random.mail@virgilio.it", "random.mail@tim.it"];
 console.log(listaMail);
+
+// controllo la lista delle mail
+var soldatino = false;
+
+for (var i = 0; i < listaMail.length; i++) {
+  if (listaMail[i] = mail) {
+    soldatino = true;
+  }
+}
+
+// stampo il risultato del controllo
